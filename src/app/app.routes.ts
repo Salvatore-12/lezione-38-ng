c7574cd10addf80b89e4c2f8de0737b23d66d0c0
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-07-22 10:46:01 +0200

    creati i vari route su app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './componenti/dashboard/dashboard.component';
+import { LoginComponent } from './componenti/login/login.component';
+import { RegisterComponent } from './componenti/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:DashboardComponent},
+    {path:'login',component:LoginComponent},
+    {path:'register',component:RegisterComponent}
+
+];
