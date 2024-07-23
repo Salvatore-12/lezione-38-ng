504f9d4f14a1b3c76efdf92af8230d553304ea97
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-07-23 10:30:44 +0200

    aggiunto su app.routes authGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,10 @@ import { RegisterComponent } from './componenti/register/register.component';
 import { Pagina1Component } from './componenti/pagina1/pagina1.component';
 import { Pagina2Component } from './componenti/pagina2/pagina2.component';
 import { Pagina3Component } from './componenti/pagina3/pagina3.component';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
-    {path:'',component:DashboardComponent,canActivate:[], children:[
+    {path:'',component:DashboardComponent,canActivate:[authGuard], children:[
         {path:'', redirectTo:'pagina1', pathMatch: 'full'},
         {path:'pagina1', component: Pagina1Component},
         {path:'pagina2', component: Pagina2Component},
