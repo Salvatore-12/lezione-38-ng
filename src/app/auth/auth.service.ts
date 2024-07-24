2b068a2fa4d948684644a263c8566ef199285b8d
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-07-24 10:13:50 +0200

    sistemazione la regitrazione

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  Apikey ='AIzaSyDzAn5R8F-bn_uZ_Bl9rqMMHJwS9jLe894'
+  signUpURL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.Apikey}`
+  signInURL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.Apikey}`
   isLoggedIn = true
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  signUp(email: string, password: string){
+    return this.http.post(this.signUpURL, {email: email, password: password, returnSecureToken: true})
+  }
+  
+  signIn(email: string, password: string){
+    return this.http.post(this.signInURL,{email: email, password: password, returnSecureToken: true})
+  }
 
 
 }
--- src/app/componenti/register/register.component.ts
+++ src/app/componenti/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { SharedModule } from '../../shared.module';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -12,12 +13,16 @@ import { SharedModule } from '../../shared.module';
 })
 export class RegisterComponent implements OnInit {
   
-  constructor(){}
+  constructor(private authService: AuthService){}
 
   ngOnInit(): void {}
 
   onSubmit(form: NgForm){
     const email = form.value
     const password = form.value
+    this.authService.signUp(email,password).subscribe(data =>{
+      console.log(data) 
+    })
+    form.reset
   }
 }
