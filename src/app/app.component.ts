5f1e3019f1f3a165d9d315f342318b1caa3591b1
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-07-24 11:21:19 +0200

    sistemazione app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './auth/auth.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,25 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'lezione-38-ng';
+
+  constructor(private authService:AuthService,
+              @Inject(PLATFORM_ID) private platformId: Object){}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const userString = localStorage.getItem('user');
+      if (userString) {
+        const user = JSON.parse(userString);
+        this.authService.createUser(user.email, user.id, user._token, user._expirationDate);
+      }
+    }
+  }
+
+  onLogout(){
+    this.authService.logout()
+  }
+
 }
