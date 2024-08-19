c3c4a68e1fb88ca271a5ed706627440b25fe6086
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-08-19 09:57:43 +0200

    aggiunto la parte back tramite la cards.service,ovvero tramite Java

---
+++ src/app/servizi/cards.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardsService {
+
+  constructor(private http:HttpClient) { }
+
+  getCardsCani(){
+    return this.http.get("http://localhost:3001/cards/cards-cane");
+  }
+
+  getCardsGatti(){
+    return this.http.get("http://localhost:3001/cards/cards-gatto")
+  }
+}
