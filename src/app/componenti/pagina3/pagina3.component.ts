5a059cf3c26547dde5e18e67fae81dd96a6be848
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-08-19 09:58:37 +0200

    aggiunte le card in pagina3

--- src/app/componenti/pagina3/pagina3.component.ts
+++ src/app/componenti/pagina3/pagina3.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardsService } from '../../servizi/cards.service';
+import { SharedModule } from '../../shared.module';
 
 @Component({
   selector: 'app-pagina3',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './pagina3.component.html',
   styleUrl: './pagina3.component.css'
 })
-export class Pagina3Component {
+export class Pagina3Component implements OnInit {
+
+CardsGatti:any
+  constructor(private service:CardsService){}
+  ngOnInit(): void {
+   this.service.getCardsGatti().subscribe(
+    data => this.CardsGatti = data,
+    err => console.error('Error loading cards:', err)
+  );
+  }
 
 }
