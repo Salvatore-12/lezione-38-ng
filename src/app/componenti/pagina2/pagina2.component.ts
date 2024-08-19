f3c445716506e8860075c112ac8ce074a5e6a5a6
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-08-19 09:58:21 +0200

    aggiunte le card in pagina2

--- src/app/componenti/pagina2/pagina2.component.ts
+++ src/app/componenti/pagina2/pagina2.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { SharedModule } from '../../shared.module';
+import { CardsService } from '../../servizi/cards.service';
 
 @Component({
   selector: 'app-pagina2',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './pagina2.component.html',
   styleUrl: './pagina2.component.css'
 })
-export class Pagina2Component {
+export class Pagina2Component implements OnInit {
+ cards:any;
+ constructor(private service:CardsService){}
+ 
+  ngOnInit(): void {
+    this.service.getCardsCani().subscribe({
+      next: data => this.cards = data,
+      error: err => console.error('Error loading cards:', err)
+    });
+  }
 
 }
